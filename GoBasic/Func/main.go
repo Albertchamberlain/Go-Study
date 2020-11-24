7f5cbfe7365fd826f2a3f68a65be2cc9ebffb91d
Albertchamberlain <2476271447@qq.com>
2020-11-24 12:02:32 +0800

    <add>:<GoBasic>

--- GoBasic/Func/main.go
+++ GoBasic/Func/main.go
@@ -23,6 +23,76 @@ func main() {
 	//不希望接收的返回值使用下划线占位
 	c, _ := demo5()
 	fmt.Println(c)
+
+	demo7("看书", "写代码", "看小胖")
+	demo8("张三", "看书", "写代码", "看小胖")
+
+	func() {
+		fmt.Println("这是匿名函数")
+	}() //括号表示调用
+
+	func(s string) {
+		fmt.Println(s, "这是匿名函数")
+	}("传递参数") //调用时传递参数
+
+	r := func(s string) int {
+		fmt.Println(s, "这是匿名函数")
+		return 110
+	}("传递参数") //调用时传递参数
+	fmt.Println(r)
+
+	var aa1 func()
+	aa1 = func() {
+		fmt.Println("执行函数")
+	} //注意此处没有括号,有括号表示调用函数,变量a就表示接收函数返回值
+	aa1() //调用函数
+
+	bb1 := func(s string) {
+		fmt.Println("执行第二个函数")
+	}
+	bb1("参数")
+
+	var a12 func()           //无参数无返回值
+	var b12 func(int)        //有一个int类型参数
+	var c15 func(int) string //有一个int类型参数和string类型返回值
+	fmt.Println(a12, b12, c15)
+	//使用定义好的函数
+	d2 := c15
+	d2(14)
+	//函数名称c也是一个变量
+	//c()
+
+	var a17 func()
+
+	a17 = b17
+	a17()
+	var c17 func()
+	c17 = a17
+	c17()
+	fmt.Printf("%p %p", a17, c17) //输出地址相同
+	a18(func(s string) {
+		fmt.Println(s)
+	})
+
+	//此时result指向返回值函数.
+	result := a19()
+	//调用函数,才能获取结果
+	fmt.Println(result())
+}
+
+func a19() func() int {
+	return func() int {
+		return 110
+	}
+}
+
+func a18(b18 func(s string)) {
+	fmt.Println("a执行")
+	b18("传递给s的内容")
+}
+
+func b17() {
+	fmt.Println("b")
 }
 
 func demo() {
@@ -50,4 +120,16 @@ func demo6() (name string, age int) {
 	name = "smallming"
 	age = 17
 	return
+}
+
+func demo7(hover ...string) {
+	for a, b := range hover {
+		fmt.Println(a, b)
+	}
+}
+func demo8(name string, hover ...string) {
+	fmt.Println(name, "的爱好是")
+	for a, b := range hover {
+		fmt.Println(a, b)
+	}
 }
