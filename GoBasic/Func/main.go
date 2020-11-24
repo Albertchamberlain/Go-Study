7fd1b60d316533e0c7874f64f61f5d745efa9147
Albertchamberlain <2476271447@qq.com>
2020-11-24 10:45:00 +0800

    <add>:<GoBasic>

--- GoBasic/Func/main.go
+++ GoBasic/Func/main.go
@@ -13,6 +13,16 @@ func main() {
 
 	a1, b1 := 1, 2
 	fmt.Println(add2(a1, b1))
+
+	demo5()
+
+	//每个返回值都接收
+	a2, b2 := demo5()
+	fmt.Println(a2, b2)
+
+	//不希望接收的返回值使用下划线占位
+	c, _ := demo5()
+	fmt.Println(c)
 }
 
 func demo() {
@@ -29,5 +39,15 @@ func add(c, d int) int {
 
 func add2(c, d int) (sum int) {
 	sum = c + d
+	return
+}
+
+func demo5() (string, int) {
+	return "xiaopang", 17
+}
+
+func demo6() (name string, age int) {
+	name = "smallming"
+	age = 17
 	return
 }
